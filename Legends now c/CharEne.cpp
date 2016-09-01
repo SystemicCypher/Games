0399486485b05b8d478fdbbd751944b49855ea43
SystemicCypher <austinloza24@yahoo.com>
2016-09-01 03:24:05 -0700

    A few last minute changes
    Not much change. I’m turning in for the night though.

--- Legends now c/CharEne.cpp
+++ Legends now c/CharEne.cpp
@@ -4,18 +4,15 @@
 #define CHARACTER_H_
 #include <string>
 #include <iostream>
+using namespace std;
 
 class Character{
 public:
 	Character();
+	//mutators
+	void change_name(const string arg)//unfinished. Ending for today ^w^
 private:
 	string name = "";
-	int HP, ATK, DEF, SPD, LVL;
-	HP = 100;
-	ATK = 10;
-	DEF = 10;
-	SPD = 10;
-	LVL = 1;
 };
 
 class Player{
