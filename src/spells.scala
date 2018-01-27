cb1fb62262019e4d2a86e4609dfc638a002484e3
SystemicCypher <austinloza24@yahoo.com>
2018-01-26 18:20:07 -0800

    Deprecated trash, new version in scala

---
+++ src/spells.scala
@@ -0,0 +1,29 @@
+object SpellType extends Enumeration{
+	type SpellType = value
+
+	val Black = Value("Black")
+	val White = Value("White")
+	val Red = Value("Red")
+}
+
+object SpellEffect extends Enumeration{
+	type SpellEffect = value
+
+	val Heal = Value("Heal")
+	val Drain = Value("Drain")
+	val Damage = Value("Damage")
+}
+
+abstract class Spell{
+	val spellType: SpellType //Black mages can't cast white magic, white mages can't cast black magic, red mages can do both but only up to a point, other classes get minimal use of a few
+	val spellLevel: Int  //Could be spell level 0-9	- non mage classes can cast up to level 2 spells
+	val spellEffects: SpellEffect //Spells can cause different effects 
+	val name: String
+}
+
+class Fireball extends Spell{
+	spellType = SpellType.Black
+	spellLevel = 1
+	spellEffects = SpellEffect.Damage
+	name = "fireball"
+}
