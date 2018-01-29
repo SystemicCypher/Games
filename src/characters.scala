5950ecacf2b7581ae3666f78f94a940e38e7c3f5
SystemicCypher <austinloza24@yahoo.com>
2018-01-29 14:35:26 -0800

    Moved classes out of characters
    I split classes from characters. I should probably split every race
    into their own file and add them into individual folders. Easier to
    look at and work through

--- src/characters.scala
+++ src/characters.scala
@@ -11,23 +11,9 @@
  *	Demon -
  *	Goblin -
  *	etc... (maybe more to be added later) [All x-ed races have been implemented, others will be soon]
- *	All of these will be creatable and playable characters
- *	Additionally, there will be a few character classes:
- *	Fighter
- *	Mage(Red/White/Black)
- *	Thief
- * 	Ranger
- *	Martial Artist
- *	Paladin
- *
- *	And NPC-only classes:
- *	Shopkeeper, Trader, Soldier, Taxi, Librarian
- * 	Educators: Sage(mage), Grandmaster(fighter/martial artist), Dragoon(Paladin), Rogue(Thief)
- *	
- *	
- */
+*/
 
-trait Character{
+class Character{
 //Values and Stats/////////////////////////////////
 	protected var name: String
 //	Hit Points
@@ -98,7 +84,7 @@ class BaseCharacter extends Character{
 
 		lvl = 1
 		exp = 0
-		
+
 	}
 
 	override def assignGender(gender: Int): Unit = {
@@ -120,7 +106,7 @@ class BaseCharacter extends Character{
 			println("It had no effect...")
 		}
 		else if(fCalc() < 0){
-			
+			val damageModifier = cons/5
 		}
 		else{
 			
@@ -232,249 +218,3 @@ class Hobbit extends Character{
 }
 */
 
-//CLASSES~~~~~~~~~~~~~~~~~~~~~~~
-/*
-trait Fighter extends Character{
-// Required stats assignment - only when created
-	abstract override def assignStats(): Unit 
-
-	abstract override def assignGender(): Unit
-	
-	abstract override def assignName(): Unit
-
-// Assign level-up stats
-	abstract override def assignLvlUpStats(lvlUpchk: () => Boolean): Unit
-
-// Level up check
-	abstract override def levelUpCheck(): Boolean
-
-// Status checks
-	abstract override def isAlive(): Boolean
-
-	abstract override def listStats(): List(Int)
-
-// HP changes
-	abstract override def hpAffected(fCalc: () => Int): Unit
-
-// Healing operations
-	abstract override def healCalc(): Int 
-
-	// Combat system operations
-	abstract override def damageCalc(): Int
-
-	abstract override def dodge(): Boolean
-
-	abstract override def attack(): Unit
-	
-	abstract override def useSpell(spell: Spell, target: Character): Unit ={
-
-	}
-}
-*/
-
-trait Mage extends Character{
-}
-
-trait BlackMage extends Mage{
-	abstract override def useSpell(spell: Spell, target: Character): Unit = {
-		if(lvl >= (spell.spellLevel * 5)){
-			if(spell.spellType == SpellType.Black) {
-				if(target.dodge){
-					println("Missed!")
-				}
-				else{
-					target.hpAffected(damageCalc)				
-				}
-			}	
-			else{
-				println("Black mages can only use black magic spells...")
-			}
-		}
-		else{
-			println("Level too low!")
-		}
-	}
-}
-
-trait RedMage extends Mage{
-	abstract override def useSpell(spell: Spell, target: Character): Unit = {
-		if(lvl >= (spell.spellLevel * 5) && spell.spellLevel < 6){
-				if(target.dodge){
-					println("Missed!")
-				}
-				else{
-					target.hpAffected(damageCalc)				
-				}
-		}
-		else{
-			println("Level too low!")
-		}
-	}
-}
-
-trait WhiteMage extends Mage{
-	abstract override def useSpell(spell: Spell, target: Character): Unit = {
-		if(lvl >= (spell.spellLevel * 5)){
-			if(spell.spellType == SpellType.White) {
-					target.hpAffected(healCalc)				
-			}	
-			else{
-				println("White mages can only use white magic spells...")
-			}
-		}
-		else{
-			println("Level too low!")
-		}
-	}
-}
-
-/*
-trait Thief extends Character{
-// Required stats assignment - only when created
-	abstract override def assignStats(): Unit 
-
-	abstract override def assignGender(): Unit
-	
-	abstract override def assignName(): Unit
-
-// Assign level-up stats
-	abstract override def assignLvlUpStats(lvlUpchk: () => Boolean): Unit
-
-// Level up check
-	abstract override def levelUpCheck(): Boolean
-
-// Status checks
-	abstract override def isAlive(): Boolean
-
-	abstract override def listStats(): List(Int)
-
-// HP changes
-	abstract override def hpAffected(fCalc: () => Int): Unit
-
-// Healing operations
-	abstract override def healCalc(): Int 
-
-// Out of combat operations
-	abstract override def speak(): String
-	// Combat system operations
-	abstract override def damageCalc(): Int
-
-	abstract override def dodge(): Boolean
-
-	abstract override def attack(): Unit
-	
-	abstract override def useSpell(spell: Spell): Unit 
-}
-
-trait Ranger extends Character{
-// Required stats assignment - only when created
-	abstract override def assignStats(): Unit 
-
-	abstract override def assignGender(): Unit
-	
-	abstract override def assignName(): Unit
-
-// Assign level-up stats
-	abstract override def assignLvlUpStats(lvlUpchk: () => Boolean): Unit
-
-// Level up check
-	abstract override def levelUpCheck(): Boolean
-
-// Status checks
-	abstract override def isAlive(): Boolean
-
-	abstract override def listStats(): List(Int)
-
-// HP changes
-	abstract override def hpAffected(fCalc: () => Int): Unit
-
-// Healing operations
-	abstract override def healCalc(): Int 
-
-// Out of combat operations
-	abstract override def speak(): String
-	// Combat system operations
-	abstract override def damageCalc(): Int
-
-	abstract override def dodge(): Boolean
-
-	abstract override def attack(): Unit
-	
-	abstract override def useSpell(spell: Spell): Unit 
-}
-
-trait MartialArtist extends Character{
-// Required stats assignment - only when created
-	abstract override def assignStats(): Unit 
-
-	abstract override def assignGender(): Unit
-	
-	abstract override def assignName(): Unit
-
-// Assign level-up stats
-	abstract override def assignLvlUpStats(lvlUpchk: () => Boolean): Unit
-
-// Level up check
-	abstract override def levelUpCheck(): Boolean
-
-// Status checks
-	abstract override def isAlive(): Boolean
-
-	abstract override def listStats(): List(Int)
-
-// HP changes
-	abstract override def hpAffected(fCalc: () => Int): Unit
-
-// Healing operations
-	abstract override def healCalc(): Int 
-
-// Out of combat operations
-	abstract override def speak(): String
-	// Combat system operations
-	abstract override def damageCalc(): Int
-
-	abstract override def dodge(): Boolean
-
-	abstract override def attack(): Unit
-	
-	abstract override def useSpell(spell: Spell): Unit 
-}
-
-trait Paladin extends Character{
-// Required stats assignment - only when created
-	abstract override def assignStats(): Unit 
-
-	abstract override def assignGender(): Unit
-	
-	abstract override def assignName(): Unit
-
-// Assign level-up stats
-	abstract override def assignLvlUpStats(lvlUpchk: () => Boolean): Unit
-
-// Level up check
-	abstract override def levelUpCheck(): Boolean
-
-// Status checks
-	abstract override def isAlive(): Boolean
-
-	abstract override def listStats(): List(Int)
-
-// HP changes
-	abstract override def hpAffected(fCalc: () => Int): Unit
-
-// Healing operations
-	abstract override def healCalc(): Int 
-
-// Out of combat operations
-	abstract override def speak(): String
-	// Combat system operations
-	abstract override def damageCalc(): Int
-
-	abstract override def dodge(): Boolean
-
-	abstract override def attack(): Unit
-	
-	abstract override def useSpell(spell: Spell): Unit 
-}
-
-*/
